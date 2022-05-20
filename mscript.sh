d71e401a45aa8c04bcd74a8880c3fd8b01a0a965
ankosyak <andykosyak.iam@gmail.com>
2022-05-21 00:32:15 +0300

    new project terminal

---
+++ mscript.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+cd happiness
+mkdir money time work
+cd time
+touch second.txt minute.txt hour.txt week.json month.json
+mkdir breakfast lunch dinner
+ls -la
+mv -t dinner/ minute.txt second.txt
+
